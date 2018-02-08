e1d232fa975e8f010037277b4112838b0b65eabd
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-08 16:48:15 +0200

    delete commented code

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-/*
-const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const mongoUrl = 'mongodb://blog-app:789456123@ds229008.mlab.com:29008/dev-blog-db'
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
-
-module.exports = Blog
-*/
 const Blog = require('./models/blog')
 
 app.use(cors())
