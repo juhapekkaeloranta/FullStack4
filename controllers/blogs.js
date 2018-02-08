7d0baa7185fbf71156ea76e19e8905229cc904ce
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-08 17:08:47 +0200

    fix BlogRouter

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,12 @@
 const blogsRouter = require('express').Router()
-const Blog = require('./models/blog')
+const Blog = require('../models/blog')
 
-app.use(cors())
-app.use(bodyParser.json())
 
-app.get('/', (request, response) => {
+blogsRouter.get('/', (request, response) => {
     response.json('hello2')
   })
 
-app.get('/api/blogs', (request, response) => {
+blogsRouter.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
@@ -16,7 +14,7 @@ app.get('/api/blogs', (request, response) => {
     })
 })
 
-app.post('/api/blogs', (request, response) => {
+blogsRouter.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
   blog
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+app.use(cors())
+app.use(bodyParser.json())
+
 const blogsRoutes = require('./controllers/blogs')
 
 app.use('/', blogsRoutes)
