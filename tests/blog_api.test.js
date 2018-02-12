472c1ee0155192f5633ed9bceadf260fd5182f4d
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-12 10:48:44 +0200

    add test seed for API test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,50 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const testSetBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
+beforeAll(async () => {
+  console.log('beforeAll:');
+  await Blog.remove({})
+  
+  let blogObj = new Blog(testSetBlogs[0])
+  await blogObj.save()
+
+  blogObj = new Blog(testSetBlogs[1])
+  await blogObj.save()
+  
+  blogObj = new Blog(testSetBlogs[2])
+  await blogObj.save()
+  
+  console.log('3 blogs seeded to db');
+})
 
 test('blogs are returned as json', async () => {
   await api
