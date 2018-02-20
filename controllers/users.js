cc097d8e296dcd2c88b3b2d001f5fdd419efb339
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-20 17:18:24 +0200

    init test for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,27 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
+usersRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body
 
-usersRouter.get('/', (request, response) => {
-    response.json('hello2')
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
+  }
+})
 
 module.exports = usersRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,171 +2,204 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
+const userHelper = require('./user_helper')
 
-const testSetBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  }
-]
+/*
+describe.skip('blog tests', () => {
+  const testSetBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
 
-beforeAll(async () => {
-  console.log('beforeAll:');
-  await Blog.remove({})
-  
-  let blogObj = new Blog(testSetBlogs[0])
-  await blogObj.save()
+  beforeAll(async () => {
+    console.log('beforeAll:');
+    await Blog.remove({})
 
-  blogObj = new Blog(testSetBlogs[1])
-  await blogObj.save()
-  
-  blogObj = new Blog(testSetBlogs[2])
-  await blogObj.save()
-  
-  console.log('3 blogs seeded to db');
+    let blogObj = new Blog(testSetBlogs[0])
+    await blogObj.save()
+
+    blogObj = new Blog(testSetBlogs[1])
+    await blogObj.save()
+
+    blogObj = new Blog(testSetBlogs[2])
+    await blogObj.save()
+
+    console.log('3 blogs seeded to db');
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('new blog may be added', async () => {
+    const newBlog = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    }
+
+    const allBlogsBefore = await helper.allBlogs()
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api
+      .get('/api/blogs')
+
+    const allBlogsAfter = await helper.allBlogs()
+
+    expect(allBlogsAfter.length).toEqual(allBlogsBefore.length + 1)
+    //expect(allBlogsAfter).toContainEqual(newBlog)
+  })
+
+  test('new blog: likes are set to 0 if empty', async () => {
+    const blogWithoutLikes = {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      __v: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    //const response = await api
+    //  .get('/api/blogs')
+
+    const response = await helper.allBlogs()
+
+    console.log(response);
+
+    const newBlog = response.find(blog =>
+      blog._id == blogWithoutLikes._id
+    )
+
+    console.log('new:');
+    console.log(newBlog._id + '\n' + blogWithoutLikes._id)
+    console.log(newBlog._id == blogWithoutLikes._id)
+    console.log(newBlog.likes === 0);
+    console.log('--END NEW');
+    expect(newBlog.likes).toEqual(0)
+  })
+
+  test('new blog: title and url compulsory', async () => {
+    const badBlogObj = {
+      author: "Robert C. Martin",
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(badBlogObj)
+      .expect(400)
+  })
+
+  test('blog can be deleted', async () => {
+    console.log('delete test')
+    const allBlogsBefore = await helper.allBlogs()
+
+    await api
+      .delete('/api/blogs/5a422a851b54a676234d17f7')
+      .expect(204)
+
+    const allBlogsAfter = await helper.allBlogs()
+
+    expect(allBlogsAfter.length).toEqual(allBlogsBefore.length - 1)
+  })
+
+  test('test helper gets test set', async () => {
+    console.log('fooTest');
+    const allBlogs = await helper.allBlogs()
+    console.log(allBlogs);
+    expect(Array.isArray(allBlogs)).toBe(true)
+    //expect(allBlogs.length).toEqual(testSetBlogs.length)
+  })
+
+  test('add like', async () => {
+    console.log('add like test')
+    const blogFromDB = await helper.findBlog('5a422aa71b54a676234d17f8')
+
+    const likesBefore = blogFromDB.likes
+    blogFromDB.likes = blogFromDB.likes + 1
+
+    const result = await api
+      .put('/api/blogs/' + blogFromDB._id)
+      .send(blogFromDB)
+
+    const blogFromDBafter = await helper.findBlog('5a422aa71b54a676234d17f8')
+
+    console.log(likesBefore)
+    console.log(blogFromDBafter.likes);
+    expect(blogFromDBafter.likes).toEqual(likesBefore + 1)
+  })
+
+  afterAll(() => {
+    console.log('closing server');
+    server.close()
+    console.log('end of afterAll');
+  })
 })
+*/
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe.only('user tests', async () => {
+  beforeAll(async () => {
+    await User.remove({})
+    const user = new User({ username: 'root', password: 'sekret' })
+    await user.save()
+  })
 
-test('new blog may be added', async () => {
-  const newBlog =  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  }
+  test('POST /api/users succeeds with a fresh username', async () => {
+    const usersBeforeOperation = await usersInDb()
 
-  const allBlogsBefore = await helper.allBlogs()
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  const response = await api
-    .get('/api/blogs')
+    const usersAfterOperation = await usersInDb()
+    expect(usersAfterOperation.length).toBe(usersBeforeOperation.length+1)
+    const usernames = usersAfterOperation.map(u=>u.username)
+    expect(usernames).toContain(newUser.username)
+  })
 
-  const allBlogsAfter = await helper.allBlogs()
-
-  expect(allBlogsAfter.length).toEqual(allBlogsBefore.length + 1)
-  //expect(allBlogsAfter).toContainEqual(newBlog)
-})
-
-test('new blog: likes are set to 0 if empty', async () => {
-  const blogWithoutLikes = {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    __v: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutLikes)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  //const response = await api
-  //  .get('/api/blogs')
-  
-  const response = await helper.allBlogs()
-
-  console.log(response);
-
-  const newBlog = response.find(blog => 
-    blog._id == blogWithoutLikes._id
-  )
-
-  console.log('new:');
-  console.log(newBlog._id + '\n' + blogWithoutLikes._id )
-  console.log(newBlog._id == blogWithoutLikes._id)
-  console.log(newBlog.likes === 0);
-  console.log('--END NEW');
-  expect(newBlog.likes).toEqual(0)
-})
-
-test('new blog: title and url compulsory', async () => {
-  const badBlogObj = {
-    author: "Robert C. Martin",
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(badBlogObj)
-    .expect(400)
-})
-
-test('blog can be deleted', async () => {
-  console.log('delete test')
-  const allBlogsBefore = await helper.allBlogs()
-
-  await api
-    .delete('/api/blogs/5a422a851b54a676234d17f7')
-    .expect(204)
-
-  const allBlogsAfter = await helper.allBlogs()
-
-  expect(allBlogsAfter.length).toEqual(allBlogsBefore.length - 1)
-})
-
-test('test helper gets test set' , async () => {
-  console.log('fooTest');
-  const allBlogs = await helper.allBlogs()
-  console.log(allBlogs);
-  expect(Array.isArray(allBlogs)).toBe(true)
-  //expect(allBlogs.length).toEqual(testSetBlogs.length)
-})
-
-test('add like', async () => {
-  console.log('add like test')
-  const blogFromDB = await helper.findBlog('5a422aa71b54a676234d17f8')
-  
-  const likesBefore = blogFromDB.likes
-  blogFromDB.likes = blogFromDB.likes + 1
-
-  const result = await api
-    .put('/api/blogs/' + blogFromDB._id)
-    .send(blogFromDB)
-
-  const blogFromDBafter = await helper.findBlog('5a422aa71b54a676234d17f8')
-
-  console.log(likesBefore)
-  console.log(blogFromDBafter.likes);
-  expect(blogFromDBafter.likes).toEqual(likesBefore + 1)
-})
-
-afterAll(() => {
-  console.log('closing server');
-  server.close()
-  console.log('end of afterAll');
+  afterAll(() => {
+    console.log('closing server');
+    server.close()
+    console.log('end of afterAll');
+  })
 })
