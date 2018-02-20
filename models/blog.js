6d8e46b58be062ba38b5da863f3b1eac1bee8cf9
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-20 16:31:32 +0200

    add model for user

--- models/blog.js
+++ models/blog.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
 
 const Blog = mongoose.model('Blog', {
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
-
-console.log(config)
-const mongoUrl = config.mongoUrl
-//mongoose.connect(mongoUrl)
-//mongoose.Promise = global.Promise
 
 module.exports = Blog
